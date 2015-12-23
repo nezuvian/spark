4af99ae8319d3364ad59f39a23bccd3eb6ca72ac
nezuvian <nezuvian@gmail.com>
2015-12-23 03:34:35 +0100

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sparkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sparkApp
+ */
+angular.module('sparkApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sparkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sparkApp
+ */
+angular.module('sparkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
